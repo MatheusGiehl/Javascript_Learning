6d82a87eaa6b7f31f688f9469f08a6f4b8ae41d4
Matheus <giehlmatheus96@gmail.com>
2024-06-29 23:02:05 -0300

    types of variables in js

--- script.js
+++ script.js
@@ -18,8 +18,21 @@ console.log("Hello World.");
 //  -> você precisa de uma variável que possa redeclarar ou 
 //  -> você precisa de uma variável acessível em qualquer lugar do programa (ou seja, globalmente)
 
-let nome = "Matheus";
-let idade = 21;
+// Tipos de váriaveis
 
-console.log(nome);
-console.log(idade);
+// Strings são úteis para guardar dados que podem ser representados em forma de texto.
+let nome = "Matheus";
+// Number armazena números 
+let idade = 21;
+// Booleano: dados que apresentam apenas duas possibilidades de valores – true (verdadeiro) ou false (falso);
+let logado = true;
+// Array: matriz que pode ser composta por diferentes tipos de dados;
+let funcoes = ["Duelista", "Iniciador", "Controlador", "Sentinela"];
+// Objeto: tipo de dado que funciona como uma entidade independente, no qual há um conjunto de atributos aninhados a uma variável
+let dados = {nome : "Matheus", idade : 21, altura: 172};
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof logado);
+console.log(typeof funcoes);
+console.log(typeof dados);
