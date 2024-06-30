2eed1bb5b3cd47a974847a61a50e2880f3537059
Matheus <giehlmatheus96@gmail.com>
2024-06-29 22:30:37 -0300

    js introdution

---
+++ script.js
@@ -0,0 +1,25 @@
+// O alert é uma das mais simples caixas de diálogo
+// sua principal função é mostrar ao usuário uma mensagem
+// para chamar essa função, basta utilizarmos o código alert(), que receberá uma string
+alert("Hello World.");
+
+// console.log()
+// Essa função é usada para imprimir mensagens de registro ou depuração no console do navegador ou no console do ambiente de desenvolvimento.
+console.log("Hello World.");
+
+// As variáveis ​​podem ser usadas para armazenar dados em um programa, como strings, números, objetos JSON ou valores booleanos. Em JavaScript, existem três tipos de variáveis ​​diferentes: var , let , e const.
+
+// Use const tanto quanto possível, a menos que você precise redeclarar ou levantar uma variável . 
+
+// Use let se estiver trabalhando com loops . 
+
+// Use var apenas se:
+//  -> você está trabalhando em código legado , 
+//  -> você precisa de uma variável que possa redeclarar ou 
+//  -> você precisa de uma variável acessível em qualquer lugar do programa (ou seja, globalmente)
+
+let nome = "Matheus";
+let idade = 21;
+
+console.log(nome);
+console.log(idade);
