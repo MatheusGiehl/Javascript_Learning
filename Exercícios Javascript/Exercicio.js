fe34ea70d3668a46bf353adcc002e31249a0b17c
Matheus <giehlmatheus96@gmail.com>
2024-07-01 21:40:51 -0300

    Exercícios com variáveis no Javascript.

---
+++ Exercícios Javascript/Exercicio.js
@@ -0,0 +1,17 @@
+/* 1. Crie uma variável para armazenar o nome
+do carro e ponha o valor Ferrari. E exiba-a. */
+
+let carName = "Ferrari";
+console.log(carName);
+
+/* 2. Crie uma variável para armazenar o preço 
+do bolo, que custa  R$ 19,99. E exiba-a. */
+
+let price = "R$ 19,99";
+console.log(price);
+
+/* 3. Corrija o código a seguir (e exiba-o).
+let "cidade" = São Paulo */
+
+let cidade = "São Paulo";
+console.log(cidade);
