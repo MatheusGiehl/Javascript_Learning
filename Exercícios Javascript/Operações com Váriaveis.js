eacaa51054545f762cde2b589bebef0033ab2043
Matheus <giehlmatheus96@gmail.com>
2024-07-15 21:11:22 -0300

    Operações com Váriaveis,strings e Template String.

---
+++ Exercícios Javascript/Operações com Váriaveis.js
@@ -0,0 +1,19 @@
+// string
+let nome = 'Matheus'; 
+let sobrenome = 'Giehl';
+
+const nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+// Template String
+const nome_Completo = `${nome} ${sobrenome}`;
+console.log(nome_Completo);
+
+// number
+let numeroUm = 5;
+let numeroDois = 10;
+
+const soma = numeroUm + numeroDois;
+
+console.log(soma);
