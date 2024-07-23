04be92ecf225a90d02b65ea70d88a28742f110f9
Matheus <giehlmatheus96@gmail.com>
2024-07-22 21:24:11 -0300

    Condicional Ternaria.

---
+++ Exercícios Javascript/CondicionalTernario.js
@@ -0,0 +1,6 @@
+let isMember = true;
+// ? = se for, : = se não for;
+let shipping = (isMember ? 2 : 5);
+
+console.log(isMember ? 'Você é membro.' : 'Você não é membro.')
+console.log('Frete: ' + shipping)
