7071878d2372205bcc90a29eb47739ea6e7e952f
Matheus <giehlmatheus96@gmail.com>
2024-07-22 21:35:20 -0300

    Switch

---
+++ Exercícios Javascript/Switch.js
@@ -0,0 +1,15 @@
+/* A condicional switch avalia uma expressão, combinando o valor da expressão para um cláusula case, e executa as instruções associadas ao case. */
+
+let profession = 'fiscal';
+
+switch (profession) {
+    case 'fiscal' :
+        console.log('Sua roupa é verde.')
+    break;
+    case 'bombeiro' :
+        console.log('Sua roupa é vermelha.')
+    break;
+    case 'policial' :
+        console.log('Sua roupa é azul.')
+    break;
+}
