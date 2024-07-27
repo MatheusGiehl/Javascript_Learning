254c1f13ba93d321c361dd8d581a059ca5979581
Matheus <giehlmatheus96@gmail.com>
2024-07-26 23:03:34 -0300

    indroducao a funcao

---
+++ Javascript Básico/funcoes.js
@@ -0,0 +1,12 @@
+// O que é uma função ?
+// Uma função é um procedimento de JavaScript - um conjunto de instruções que executa uma tarefa ou calcula um valor.
+
+// Declaração de função.
+
+function frase() {
+    console.log("Essa função mostra uma frase no console.")
+} 
+
+// Usando uma função.
+
+frase()
