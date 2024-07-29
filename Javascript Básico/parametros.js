b68b38a3eb846916d69d8bda786947f93d996607
Matheus <giehlmatheus96@gmail.com>
2024-07-29 19:22:48 -0300

    parametros

---
+++ Javascript Básico/parametros.js
@@ -0,0 +1,17 @@
+
+
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log('Resultado: ' + resultado)
+}
+
+somar(10, 15)
+
+// Entrada -> Processamento -> Saída.
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+}
+
+ let completo = nomeCompleto("Matheus", "Giehl")
+
+ console.log(completo)
