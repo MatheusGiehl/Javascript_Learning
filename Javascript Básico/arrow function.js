491e82aa28d41322136aa3f2c723707e32263fe8
Matheus <giehlmatheus96@gmail.com>
2024-07-30 20:21:42 -0300

    Arrow Functions.

---
+++ Javascript Básico/arrow function.js
@@ -0,0 +1,10 @@
+const somar = (x, y) => {
+    let resultado = x + y;
+    return resultado;
+}
+
+console.log(somar(10, 20));
+
+const sobrenome = (sob) => `${'Matheus'} ${sob}`;
+
+console.log(sobrenome('Giehl'));
